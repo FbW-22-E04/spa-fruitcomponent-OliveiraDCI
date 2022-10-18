49dc27a042b28fbe374aa98579fac3c013777bb4
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-10-18 16:37:05 +0200

    Click functionality implemented.

--- fruits-react-app/src/App.js
+++ fruits-react-app/src/App.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
+
 function App() {
+  const [apple, setApple] = useState("red");
+  const [mango, setMango] = useState("red");
+  const [banana, setBanana] = useState("red");
+  const [lemon, setLemon] = useState("red");
+
+  let colorArr = ["red", "orange", "yellow", "purple", "green"];
+
+  let randomIdx = () => Math.floor(Math.random() * colorArr.length);
+  const handleColor = () => colorArr[randomIdx()];
+  const handleDelete = (e) => (e.target.parentElement.innerHTML = "");
+  const handleDeleteAll = (e) => (e.target.parentElement.innerHTML = "");
+
   return (
     <div className="App">
-      <h1>Hello there!</h1>
+      <div>
+        <div>Apple, color = {apple}</div>
+        <button onClick={() => setApple(handleColor)}>change color</button>
+        <button onClick={(e) => handleDelete(e)}>delete me</button>
+      </div>
+      <div>
+        <div>Mango, color = {mango}</div>
+        <button onClick={() => setMango(handleColor)}>change color</button>
+        <button onClick={(e) => handleDelete(e)}>delete me</button>
+      </div>
+      <div>
+        <div>Banana, color = {banana}</div>
+        <button onClick={() => setBanana(handleColor)}>change color</button>
+        <button onClick={(e) => handleDelete(e)}>delete me</button>
+      </div>
+      <div>
+        <div>Lemon, color = {lemon}</div>
+        <button onClick={() => setLemon(handleColor)}>change color</button>
+        <button onClick={(e) => handleDelete(e)}>delete me</button>
+      </div>
+      <br />
+      <button onClick={(e) => handleDeleteAll(e)}>DELETE ALL</button>
     </div>
   );
 }
